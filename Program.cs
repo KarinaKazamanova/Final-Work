068dbc06d7cdb4e51dbd39c3c4967f1c83a3840b
Karina <k5524392@gmail.com>
2022-09-05 18:49:36 +0300

    решение кр 1 этап

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿System.Console.WriteLine("Введите массив");
+string? coordinates = Console.ReadLine();
+string  coordinates1 = Replace (coordinates, ',' ,' ');
+string  coordinates2 = Delete (coordinates1, ')');
+string  coordinates3 = Delete (coordinates2, '(');  
+string[] figure = coordinates3.Split(" ");
+string [] b = NoMoreThanThree ( a);
+Print (b);
+string [] NoMoreThanThree (string [] a)
+{
+    string [] b = new string [0];
+    for (int i = 0; i <a.Length; i ++)
+    {
+        if (a[i].Length <4)
+        {
+            b = b.Append (a[i]).ToArray();
+        }
+    }
+    return b;
+}
+void Print (string [] a)
+{
+    for (int i = 0; i <a.Length; i ++)
+    {
+        System.Console.Write($"'{a[i]}', ");
+    }
+}
